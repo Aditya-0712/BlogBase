31751f21fa994b3e2f5408442b7e0afee8198653
w3project <adityabatgeri@gmail.com>
2023-06-30 12:55:57 +0530

    pc view started

--- public/index.js
+++ public/index.js
@@ -6,3 +6,42 @@ ham.onclick = function()
     ham.classList.toggle("hamopen");
     sidebar.classList.toggle("sideopen");
 }
+
+var linq = document.querySelectorAll(".pctopbar .under a");
+var under = document.querySelectorAll(".pctopbar .under div");
+
+linq[0].onmouseover = function()
+{
+    under[0].style.transform = "translateX(0%)";
+}
+linq[0].onmouseout = function()
+{
+    under[0].style.transform = "translateX(-100%)";
+}
+
+linq[1].onmouseover = function()
+{
+    under[1].style.transform = "translateX(0%)";
+}
+linq[1].onmouseout = function()
+{
+    under[1].style.transform = "translateX(-100%)";
+}
+
+linq[2].onmouseover = function()
+{
+    under[2].style.transform = "translateX(0%)";
+}
+linq[2].onmouseout = function()
+{
+    under[2].style.transform = "translateX(-100%)";
+}
+
+linq[3].onmouseover = function()
+{
+    under[3].style.transform = "translateX(0%)";
+}
+linq[3].onmouseout = function()
+{
+    under[3].style.transform = "translateX(-100%)";
+}
