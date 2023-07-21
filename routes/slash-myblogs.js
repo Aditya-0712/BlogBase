f564024b649d15b4d9502a8bfe8b80720977e892
w3project <adityabatgeri@gmail.com>
2023-07-21 20:38:41 +0530

    Only likes and styling remaining

--- routes/slash-myblogs.js
+++ routes/slash-myblogs.js
@@ -1,9 +1,29 @@
 const exp = require('express');
+const addProfile = require('../models/credentials');
+const newBlog = require('../models/blog');
 const route = exp.Router();
-const path = require('path');
 
 route.get('/myblogs', (req,res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'myBlogs.html'));
-})
+    const currentUser = addProfile.getCurrentUser();
+    const blogIDs = currentUser[0].BLOGS;
+    const allBlogs = newBlog.getAllBLogs();
+
+    const myBlogs = [];
+
+    for (let i=0; i<blogIDs.length; i++)
+    {
+        for (let j=0; j<allBlogs.length; j++)
+        {
+            if (blogIDs[i] == allBlogs[j].ID)
+            {
+                myBlogs.push(allBlogs[j]);
+            }
+        }
+    }
+
+    const data = {arr:myBlogs};
+
+    res.render('myBlogs', data);
+});
 
 module.exports = route;
